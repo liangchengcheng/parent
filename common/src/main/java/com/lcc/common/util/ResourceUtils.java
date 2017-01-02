d0772b3f29ae5c95d656a3081d425735a95ef942
liangchengcheng <1038127753@qq.com>
2017-01-02 15:41:03 +0800

    add util class file
    add util class file

---
+++ common/src/main/java/com/lcc/common/util/ResourceUtils.java
@@ -0,0 +1,53 @@
+package com.lcc.common.util;
+
+import java.util.ResourceBundle;
+import java.text.MessageFormat;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 资源文件工具类
+ * Created by lcc on 2017/1/2.
+ */
+public class ResourceUtils {
+
+    private ResourceBundle resourceBundle;
+
+    private ResourceUtils(String resource) {
+        resourceBundle = ResourceBundle.getBundle(resource);
+    }
+
+    /**
+     * 获取资源
+     *
+     * @param resource 资源
+     * @return 解析
+     */
+    public static ResourceUtils getResource(String resource) {
+        return new ResourceUtils(resource);
+    }
+
+    /**
+     * 根据key取得value
+     *
+     * @param key  键值
+     * @param args value中参数序列，参数:{0},{1}...,{n}
+     */
+    public String getValue(String key, Object... args) {
+        String temp = resourceBundle.getString(key);
+        return MessageFormat.format(temp, args);
+    }
+
+    /**
+     * 获取所有资源的Map表示
+     *
+     * @return 资源Map
+     */
+    public Map<String, String> getMap() {
+        Map<String, String> map = new HashMap<String, String>();
+        for (String key : resourceBundle.keySet()) {
+            map.put(key, resourceBundle.getString(key));
+        }
+        return map;
+    }
+}
---
+++ common/src/main/java/com/lcc/common/util/ThreadUtil.java
@@ -0,0 +1,32 @@
+package com.lcc.common.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by lcc on 2017/1/2.
+ */
+public class ThreadUtil {
+    @SuppressWarnings("rawtypes")
+    private static final ThreadLocal ctx = new ThreadLocal();
+
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    public static void put(Object key, Object value) {
+        Map m = (Map) ctx.get();
+        if (m == null) {
+            m = new HashMap();
+        }
+        m.put(key, value);
+        ctx.set(m);
+    }
+
+    public static Object get(Object key) {
+        @SuppressWarnings("rawtypes")
+        Map m = (Map) ctx.get();
+        if (m != null) {
+            return m.get(key);
+        }
+        return null;
+    }
+
+}
