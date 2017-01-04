5c73fada31096c4d7acb16fd9e55f698347d9ea8
liangchengcheng <1038127753@qq.com>
2017-01-04 20:18:23 +0800

    add service project file
    add service project file

--- service_user/src/main/java/com/lcc/service/UserServiceImpl.java
+++ service_user/src/main/java/com/lcc/service/UserServiceImpl.java
@@ -13,11 +13,12 @@ import java.util.List;
 public class UserServiceImpl implements UserService {
 
     private final Logger LOG = LoggerFactory.getLogger(this.getClass());
+
     @Autowired
     private UserDao userDao;
+
     @Autowired
     private RedisCache cache;
-
 
     @Override
     public List<User> getUserList(int offset, int limit) {
